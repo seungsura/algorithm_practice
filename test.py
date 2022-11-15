64fbf9ece8ae6e3b4966a42accced206855a82f7
seungsura <seungsura@gmail.com>
2022-11-16 08:40:52 +0900

    a
    a

--- test.py
+++ test.py
@@ -1,25 +1,22 @@
 import sys
 input = sys.stdin.readline
-from collections import defaultdict
 
-n, m = map(int, input().split())
-list1 = []
-list2 = []
-dict_list = defaultdict(list)
 
-for i in range(n+m):
-    list1.append(str(input()))
+def recursion(s, l, r):
+    global cnt
+    cnt += 1
+    if l >= r: 
+        return 1
+    elif s[l] != s[r]: 
+        return 0
+    else: 
+        return recursion(s, l+1, r-1)
 
-for i in list1:
-    if i in dict_list:
-        dict_list[i] += 1
-    else:
-        dict_list[i] = 1
-        
-    if dict_list[i] >= 2:
-        list2.append(i)
-        
-list2.sort()
-print(len(list2))
-print(''.join(list2), end= '')
+def isPalindrome(s):
+    return recursion(s, 0, len(s)-1)
 
+t = int(input())
+for _ in range(t):
+    cnt = 0
+    s = str(input().strip())
+    print(isPalindrome(s), cnt)
