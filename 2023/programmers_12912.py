f9ea84e33381b0eb3905bd9fafc58a2c2a39e0ce
seungsura <90497686+seungsura@users.noreply.github.com>
2023-05-12 20:46:03 +0900

    Create programmers_12912.py

---
+++ 2023/programmers_12912.py
@@ -0,0 +1,15 @@
+def solution(a, b):
+    
+    answer = 0
+    
+    if a > b :
+        for i in range(b, a+1 , 1):
+            answer += i
+    elif a == b:
+        answer == a
+    
+    for i in range(a, b+1, 1):
+        answer += i    
+    
+    
+    return answer
